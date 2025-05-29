729249abf445e331b2b9a3ca4d7260bb2ff20d59
Emma Kadlecek <kadlecekemma@gmail.com>
2025-05-29 08:47:02 -0700

    better ui?

--- frontend/lib/mainpage.dart
+++ frontend/lib/mainpage.dart
@@ -31,10 +31,48 @@ class Mainpage extends StatelessWidget {
 			mainAxisAlignment: MainAxisAlignment.center,
 			children: [
 			Fullcounterwidget(title: "Auton L3"),
+
 			Padding(padding: EdgeInsets.only(right: 10)),
 			Fullcounterwidget(title: "Auton L4",)
 			],
-			)
+			),
+			Padding(padding: EdgeInsets.all(10)),
+			Row(
+			mainAxisAlignment: MainAxisAlignment.center,
+			children: [
+			Fullcounterwidget(title: "Teleop L2"),
+			Padding(padding: EdgeInsets.only(right: 10)),
+			Fullcounterwidget(title: "Teleop L2")
+			],
+			),
+			Row(
+			mainAxisAlignment: MainAxisAlignment.center,
+			children: [
+			Fullcounterwidget(title: "Teleop L3"),
+			Padding(padding: EdgeInsets.only(right: 10)),
+			Fullcounterwidget(title: "Teleop L4",)
+			],	
+			),
+			Padding(padding: EdgeInsets.all(10)),
+			Row(
+			mainAxisAlignment: MainAxisAlignment.center,
+			children: [
+			Fullcounterwidget(title: "Algae Processor"),
+			Padding(padding: EdgeInsets.only(right: 10)),
+			Fullcounterwidget(title: "Algae Barge")
+			],
+			),
+			Padding(padding: EdgeInsetsGeometry.all(10)),
+			Row(
+			mainAxisAlignment: MainAxisAlignment.center,
+			children: [
+			Fullcounterwidget(title: "Speed"),
+			Padding(padding: EdgeInsets.only(right: 10)),
+			Fullcounterwidget(title: "Fouls")
+			],
+			),
+			Padding(padding: EdgeInsetsGeometry.all(2)),			Center(child: Text("Defense", textScaler: TextScaler.linear(1.5),),),
+			Center(child: Checkbox(value: false, onChanged: (bool? val){}),)
 			],
 		)
 		),
