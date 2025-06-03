5d0827a98b0f4acd77223278fd3ec2d740550ede
Emma Kadlecek <kadlecekemma@gmail.com>
2025-06-03 12:15:13 -0700

    Please merge the caching pr

---
+++ backend/src/tba/main.py
@@ -0,0 +1,19 @@
+API_URL = "https://www.thebluealliance.com/api/v3/"
+import requests
+from typing import Any
+
+def tba_api_call(endpoint: str, etag: str | None = None) -> tuple[Any | bool, str | None]:
+    if etag is not None:
+        data = requests.get(f"{API_URL}/{endpoint}", headers={
+            "Etag": "",
+            "X-TBA-Auth-Key": "",
+            "If-None-Match": "",
+            })
+        if data.status_code == 304:
+            # no change
+            return ("","foo") # I need the caching pr to be merged so it returns nonesense for now
+
+        if data.status_code == 200:
+            return (data.headers.get("Etag"),data.text)
+    return ("deez", "yk what")
+
