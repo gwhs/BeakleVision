1b6243dd4e628ac2d6a9cb9c1b2be6c3d8e53323
Emma Kadlecek <kadlecekemma@gmail.com>
2025-06-03 12:29:22 -0700

    fix ruff errors

--- backend/src/tba/main.py
+++ backend/src/tba/main.py
@@ -1,14 +1,19 @@
 API_URL = "https://www.thebluealliance.com/api/v3/"
-import requests
 from typing import Any
+
+import requests
+
 
 def tba_api_call(endpoint: str, etag: str | None = None) -> tuple[Any | bool, str | None]:
     if etag is not None:
-        data = requests.get(f"{API_URL}/{endpoint}", headers={
+        data = requests.get(f"{API_URL}/{endpoint}", 
+            headers={
             "Etag": "",
             "X-TBA-Auth-Key": "",
             "If-None-Match": "",
-            })
+            },
+            timeout=10
+            )
         if data.status_code == 304:
             # no change
             return ("","foo") # I need the caching pr to be merged so it returns nonesense for now
