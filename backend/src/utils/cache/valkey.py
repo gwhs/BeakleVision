1c699e65e873119b78a5243914ea19dbd4094d57
Emma Kadlecek <kadlecekemma@gmail.com>
2025-06-06 14:09:30 -0700

    fixes

--- backend/src/utils/cache/valkey.py
+++ backend/src/utils/cache/valkey.py
@@ -1,29 +1,16 @@
-# I SPENT 2 HOURS FIGURING THIS OUT FML
 from typing import Any
 
 import jsoncache
-import valkey
-from valkey.exceptions import (
-    ConnectionError as ValkeyConnectionError,  # I HATE THIS STUPID BS WHY ARE THERE MULTIPLE CONNECTION ERRORS ISWTG THIS IS THE STUPIDEST BUG EVER,
-)
+import orjson
+import valkey.asyncio as valkey
 
 conn = valkey.Valkey()
 
-
 async def set_cache(data: dict[str, Any], title: str, write_json: bool) -> None:
 
-    try:
-        await conn.ping()
-    except ValkeyConnectionError:
-        print("ERR: Valkey server is down")
-        return
-
-
-    return None
-
     if write_json:
-        jsoncache.write_cache_json(data,title)
-    dat = str(await conn.set(title,str(data)))
+       await jsoncache.write_cache_json(data,title)
+    dat = str(await conn.set(title,orjson.dumps(data)))
     if dat is None:
         return None
     elif dat is not None:
@@ -35,7 +22,7 @@ async def get_cache(data_name: str, check_json: bool) -> str | None:
         exit(1)
 
     if check_json:
-        json_data = jsoncache.read_cache_json(data_name)
+        json_data = await jsoncache.read_cache_json(data_name)
         if json_data is not None:
             return json_data
     dat = conn.get(data_name)
