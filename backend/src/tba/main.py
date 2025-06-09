6bb2688f480ced77d3767097dd8e2ae09720418e
R1shy <kadlecekemma@gmail.com>
2025-06-09 05:47:05 -0700

    tba api call (#10)
    Co-authored-by: Noelle Wang <73260931+No767@users.noreply.github.com>

---
+++ backend/src/tba/main.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+import aiohttp
+from yarl import URL
+
+API_URL = URL("https://www.thebluealliance.com/api/v3/")
+
+async def tba_api_call(endpoint: str, etag: str | None = None) -> tuple[Any | bool, str | None]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{API_URL}{endpoint}") as response:
+            async with session.get('http://python.org') as response:
+                print(f"CODE: {response.status}")
+                print(f"YAP: {await response.text()}")
+                return ("to", "do")
